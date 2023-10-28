a250c80808fe09b3449f0b1ead3765242929e754
octavioalamo <tavmsn@gmail.com>
2023-10-28 15:21:12 +0200

    Changed modify min.io docker build

--- min.io/init_bucket.sh
+++ min.io/init_bucket.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/scripts/wait-for-it.sh bucket:9000
+/init_scripts/wait-for-it.sh bucket:9000
 /usr/bin/mc alias set myminio http://bucket:9000 admin S1ngu1ar;
 /usr/bin/mc mb myminio/perfume-images;
 /usr/bin/mc anonymous set public myminio/perfume-images;
